9409761a03b73445a4d9bb973cec3e81361ae005
mapachogithub <revistacienciometrik@gmail.com>
2025-06-13 15:41:58 -0500

    sin admin backend pero de resto ok
    sin admin backend pero de resto ok

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// // JS/firebase-config.js
+// const firebaseConfig = {
+//     apiKey: "TU_API_KEY",
+//     authDomain: "TU_AUTH_DOMAIN",
+//     projectId: "TU_PROJECT_ID",
+//     storageBucket: "TU_STORAGE_BUCKET",
+//     messagingSenderId: "TU_MESSAGING_SENDER_ID",
+//     appId: "TU_APP_ID"
+// };
+
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// // export const db = firebase.firestore(); // Si usas módulos, puedes exportar db
